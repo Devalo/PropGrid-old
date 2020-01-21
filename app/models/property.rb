fd6206d151f708edda4c90aea2c07a4a7733aeeb
Stephan Bakkelund <stephan@devalo.no>
2020-01-21 20:33:30 +0100

    started validations. These might be changed at any point

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,19 @@
 class Property < ApplicationRecord
+    validates :address, length: { in: 2..100}
+    validates :property_number, length: { maximum: 1000}
+    validates :postal_code, length: { in: 3..4}
+    validates :city, length: { in: 2..50}
+    validates :property_image, length: {maximum: 100}
+    validates :rent, numericality: true
+
 end
+
+
+#t.string "name"
+#    t.string "address"
+#    t.string "property_number"
+#    t.integer "postal_code"
+#    t.string "city"
+#    t.string "property_image"
+#    t.integer "rent"
+#    t.string "tenant"
