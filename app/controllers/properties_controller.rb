b2a1257930834e7abfe13b6effd180896d1d3a37
Stephan Bakkelund <stephan@devalo.no>
2020-02-18 20:20:56 +0100

    added login to navbar

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,4 +1,5 @@
 class PropertiesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_property, only: [:show, :edit, :update, :destroy]
     def index 
         @properties = Property.order('created_at DESC')
