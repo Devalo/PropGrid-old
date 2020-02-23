726bb6bebb89ebf11b06c8addb91e32adc367dd2
Stephan Bakkelund <stephan@devalo.no>
2020-02-23 19:46:53 +0100

    added devise views, fixed styling on login

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
     resources :property_units
   end
   devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-  root to: "static_pages#index"
+  root to: "properties#index"
 end
