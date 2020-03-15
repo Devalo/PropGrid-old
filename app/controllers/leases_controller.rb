5ef1b0b83ba3cce124805623f608d66c013146c5
Stephan Bakkelund <stephan@devalo.no>
2020-03-15 08:08:03 +0100

    propert new action show in relationship to unit

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -1,22 +1,28 @@
 class LeasesController < ApplicationController
-  #before_action :get_property_unit
-  #before_action :set_lease, only: [:show, :edit, :update, :destroy]
+  before_action :get_property_unit
+  before_action :set_lease, only: [:show, :edit, :update, :destroy]
 
   def index
     @lease = Lease.all
   end
 
   def new
+    #build_lease because of the one_to_one relationship
+    #which differs from "property_unit.leases.build" since property unit
+    #only has one lease.
+    @lease = @property_unit.build_lease
+
   end
 
   private
+  #Make @property_unit available in class
   def get_property_unit
-  #  @lease = Lease.find(params[:propery_unit_id])
+    @property_unit = PropertyUnit.find(params[:property_unit_id])
   end
 
   def set_lease
   #  @lease = @property.property_units.find(params[:id])
-  #  @lease = @property_units.lease.find(params[:id])
+    @lease = @property_units.lease.find(params[:id])
   end
 
   def proper_user
