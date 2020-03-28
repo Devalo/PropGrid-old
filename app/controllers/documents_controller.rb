b87856a921caf3615b3bd50c57f3847b7d5b08fb
Stephan Bakkelund <stephan@devalo.no>
2020-03-28 16:14:23 +0100

    proper foreign key on unit

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,6 +1,11 @@
 class DocumentsController < ApplicationController
     before_action :set_document, only: [:show, :edit, :update, :destroy]
+    before_action :get_property_unit
+    before_action :get_property
+
     require "image_processing/mini_magick"
+
+
 
     # GET /documents
     # GET /documents.json
@@ -16,7 +21,10 @@ class DocumentsController < ApplicationController
 
     # GET /documents/new
     def new
+      @document = @property.property_unit.documents.build
       @document = Document.new
+
+      @property_unit = @property.property_units.build
     end
 
     # GET /documents/1/edit
@@ -27,6 +35,8 @@ class DocumentsController < ApplicationController
     # POST /documents.json
     def create
       @document = Document.new(document_params)
+      @document = @property_unit.documents.build(document_params)
+      #@property_unit = @property.property_units.build(property_unit_params.merge(user_id: current_user.id))
 
       uploaded_item = params[:document][:doc]
       file_type = uploaded_item.content_type
@@ -41,7 +51,7 @@ class DocumentsController < ApplicationController
       respond_to do |format|
         if @document.save
 
-          format.html { redirect_to @document, notice: 'Document was successfully created.' }
+          format.html { redirect_to root_path, notice: 'Document was successfully created.' }
           format.json { render :show, status: :created, location: @document }
         else
           format.html { render :new }
@@ -80,8 +90,18 @@ class DocumentsController < ApplicationController
         @document = Document.find(params[:id])
       end
 
+      def get_property
+        @property = Property.find(params[:property_id])
+      end
+
+      def get_property_unit
+        @property_unit = PropertyUnit.find(params[:property_unit_id])
+      end
+
+
       # Never trust parameters from the scary internet, only allow the white list through.
+
       def document_params
-        params.require(:document).permit(:title, :url, :doc)
+        params.require(:document).permit(:title, :doc)
       end
 end
