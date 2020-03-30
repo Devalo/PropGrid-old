c44b019268a6c254733736ea52b0ba3875683e6e
Stephan Bakkelund <stephan@devalo.no>
2020-03-30 06:16:28 +0200

    can see personal and landlord info

--- app/controllers/tenant_space_controller.rb
+++ app/controllers/tenant_space_controller.rb
@@ -1,6 +1,9 @@
 class TenantSpaceController < ApplicationController
   before_action :authenticate_tenant!
   def index
+    @property_unit = PropertyUnit.find_by(tenant: current_tenant.id)
+    p @property = Property.find_by(id: @property_unit.property_id)
+    @tenant = Tenant.find_by(id: current_tenant.id)
 
   end
 end
